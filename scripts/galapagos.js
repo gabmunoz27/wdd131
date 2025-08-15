2e5d95583170445d6423a1f54d7468793086ffa6
gabmunoz27 <rusgab27@gmail.com>
2025-08-15 15:21:22 -0500

    ecuador country page

--- scripts/galapagos.js
+++ scripts/galapagos.js
@@ -1,20 +1,15 @@
-// Footer Year and Last Modified
 document.getElementById('year').textContent = new Date().getFullYear();
 document.getElementById('lastModified').textContent = document.lastModified;
 
-// Weather Variables
 const temperature = parseFloat(document.getElementById('temp').textContent);
 const windSpeed = parseFloat(document.getElementById('wind').textContent);
 
-// Wind Chill Calculation Function
 function calculateWindChill(temp, speed) {
   return (13.12 + 0.6215 * temp - 11.37 * Math.pow(speed, 0.16) + 0.3965 * temp * Math.pow(speed, 0.16)).toFixed(1);
 }
 
-// Check conditions before calling function
-let windChillValue = "N/A";
+ let windChillValue = "N/A";
 if (temperature <= 10 && windSpeed > 4.8) {
   windChillValue = calculateWindChill(temperature, windSpeed) + " °C";
 }
-
 document.getElementById('chill').textContent = windChillValue;
